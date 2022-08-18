4a08a36ce9b214d8732516fffed55fb79584feba
武俊彪 <1083023450@qq.com>
2022-08-18 20:58:33 +0800

    0818项目网关结束

--- gmall-admin/src/main/java/com/atguigu/config/MybatisPlusConfig.java
+++ gmall-admin/src/main/java/com/atguigu/config/MybatisPlusConfig.java
@@ -1,16 +1,18 @@
 /**
  * Copyright (c) 2016-2019 谷粒开源 All rights reserved.
- *
+ * <p>
  * https://www.guli.cloud
- *
+ * <p>
  * 版权所有，侵权必究！
  */
 
 package com.atguigu.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,17 +23,42 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MybatisPlusConfig {
+    /*
+           使用mybatis-plus步骤：
 
-    /**
-     * 分页插件
-     */
+           1. 配置目录中引入映射文件
+           2. 在启动类上添加@MapperScan扫描所有mapper/dao接口
+           3. 编写XxxMapper接口实现BaseMapper\<T\>即可
+           4. 编写XxxService接口继承IService\<T\>，编写xxxServiceImpl实现类继承ServiceImpl<XxxMapper,  XxxEntity>
+           5. 查询分页要添加分页过滤器：
+       */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+
+        //  paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        //TODO 蒙的
+        paginationInterceptor.setSqlParser(new JsqlParserCountOptimize());
+        return paginationInterceptor;
     }
+
 
     @Bean
     public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
     }
+
+  /*  @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }*/
+
+
 }
--- gmall-oms/src/main/java/com/atguigu/gmall/oms/GmallOmsApplication.java
+++ gmall-oms/src/main/java/com/atguigu/gmall/oms/GmallOmsApplication.java
@@ -1,9 +1,17 @@
 package com.atguigu.gmall.oms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.atguigu.gmall.oms.mapper")
+@EnableSwagger2
+@EnableFeignClients
+@RefreshScope
 public class GmallOmsApplication {
 
     public static void main(String[] args) {
